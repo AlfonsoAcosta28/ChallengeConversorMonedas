d9097e19f299b89898fe062e4073fca54b7a15c1
Alfonso Acosta <alfonsoacosta207@gmail.com>
2024-05-03 17:06:58 -0600

    Prueba

--- src/com/alura/conversormonedas/principal/Principal.java
+++ src/com/alura/conversormonedas/principal/Principal.java
@@ -3,5 +3,6 @@ package com.alura.conversormonedas.principal;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		System.out.println("Si");
 	}
 }
